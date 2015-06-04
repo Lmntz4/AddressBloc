77a0912219365f2a17a3c0ddbddb778053ca288d
Jeff Rogers <lmntz23@gmail.com>
2015-06-04 04:31:29 +0000

    Assignment, AddressBloc Ruby Models, Completed

--- specs/address_book_specs.rb
+++ specs/address_book_specs.rb
@@ -34,4 +34,25 @@ RSpec.describe AddressBook do
        expect(new_entry.email).eql? 'augusta.king@lovelace.com'
      end
    end
+
+   context ".remove_entry" do
+    it "removes a single entry from the address book" do
+      book = AddressBook.new
+      entry = book.add_entry('JDR', '1234567890', 'jdr@jdr.com')
+      book.remove_entry(entry)
+
+      expect(book.entries.size).eql? 0
+    end
+
+    it "removes a single entry from an address book with multiple entries" do
+      book = AddressBook.new
+      
+      5.times do book.add_entry('abc', 'number', 'abc@email.com')
+      end
+      entry = book.add_entry('JDR', '1234567890', 'jdr@jdr.com')
+      book.remove_entry(entry)
+
+      expect(book.entries.size).eql? 5
+    end
+  end
 end
